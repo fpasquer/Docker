f2d8aebcd9d31b819d39502ee613ae67291c4980
Florian PASQUEREAU <fpasquer@e1r5p12.42.fr>
2017-10-26 08:28:22 +0200

    done cleaned

--- 01_dockerfiles/ex03/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-docker-machine ip Char
-echo "set bdd type with SQLite3 "
-echo "set localhost with the ipadress of Char"
-echo "give all informations in admin account setting"
-docker run -it -p 3000:3000 gogs
